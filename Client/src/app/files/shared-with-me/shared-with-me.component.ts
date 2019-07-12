f58343f828d7d7f165637f371fee7ad0ae689d34
catalry <catalry@users.noreply.github.com>
2019-07-12 16:59:45 +0200

    Issue #9 adding license

--- Client/src/app/files/shared-with-me/shared-with-me.component.ts
+++ Client/src/app/files/shared-with-me/shared-with-me.component.ts
@@ -1,3 +1,12 @@
+/*
+EasyShare - a module of CIRCABC
+Copyright (C) 2019 European Commission
+
+This file is part of the "EasyShare" project.
+
+This code is publicly distributed under the terms of EUPL-V1.2 license,
+available at root of the project or at https://joinup.ec.europa.eu/collection/eupl/eupl-text-11-12.
+*/
 import { Component, OnInit } from '@angular/core';
 import { faLock } from '@fortawesome/free-solid-svg-icons';
 import { FileInfoUploader, SessionService, FileService, UsersService, UserInfo, FileInfoRecipient } from '../../openapi';
