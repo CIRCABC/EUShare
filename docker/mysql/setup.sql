9d2e814abed3cd21296c119fe01c47e062cebc8d
catalry <catalry@users.noreply.github.com>
2019-08-05 11:45:23 +0200

    Issue #18: Removing default user drop and replacing localhost by 127.0.0.1

--- docker/mysql/setup.sql
+++ docker/mysql/setup.sql
@@ -1,4 +1,3 @@
 CREATE DATABASE easyshare DEFAULT CHARACTER SET utf8;
-DROP USER ''@'localhost';
 CREATE USER 'easyshare'@'%' IDENTIFIED BY 'easyshare';
 GRANT ALL PRIVILEGES ON easyshare.* TO 'easyshare'@'%';
