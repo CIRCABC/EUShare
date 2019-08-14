f24bf4f30025682827c63cc01f3320cc56e5a7f8
catalry <catalry@users.noreply.github.com>
2019-08-14 15:11:01 +0200

    Issue #15: Adding license

--- Client/src/app/common/formComponents/email-input/email-input.component.ts
+++ Client/src/app/common/formComponents/email-input/email-input.component.ts
@@ -1,3 +1,12 @@
+/*
+EasyShare - a module of CIRCABC
+Copyright (C) 2019 European Commission
+
+This file is part of the "EasyShare" project.
+
+This code is publicly distributed under the terms of EUPL-V1.2 license,
+available at root of the project or at https://joinup.ec.europa.eu/collection/eupl/eupl-text-11-12.
+*/
 import { Component, OnInit, forwardRef, ElementRef, ViewChild, Self } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR, NG_VALIDATORS, NgControl, Validators, ValidationErrors, ValidatorFn } from '@angular/forms';
 
