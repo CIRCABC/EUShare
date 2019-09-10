9525c15c57ff48b9fcc340280788731582077ba4
catalry <catalry@users.noreply.github.com>
2019-09-10 13:13:30 +0200

    Issue #11 : Adding license

--- client/angular/src/app/common/download-file-row-container/download-file-row-container.component.spec.ts
+++ client/angular/src/app/common/download-file-row-container/download-file-row-container.component.spec.ts
@@ -1,3 +1,12 @@
+/*
+EasyShare - a module of CIRCABC
+Copyright (C) 2019 European Commission
+
+This file is part of the "EasyShare" project.
+
+This code is publicly distributed under the terms of EUPL-V1.2 license,
+available at root of the project or at https://joinup.ec.europa.eu/collection/eupl/eupl-text-11-12.
+*/
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DownloadFileRowContainerComponent } from './download-file-row-container.component';
