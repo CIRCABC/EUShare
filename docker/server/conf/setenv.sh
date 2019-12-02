e071ef0dc5bf10bda5ba2eab16195b5adf70d9c1
catalry <catalry@users.noreply.github.com>
2019-12-02 15:31:15 +0100

    Issue #52 : changing debugging port to 8886

--- docker/server/conf/setenv.sh
+++ docker/server/conf/setenv.sh
@@ -1,1 +1,1 @@
-set JAVA_OPTS=-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=88888
+set JAVA_OPTS=-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8886
