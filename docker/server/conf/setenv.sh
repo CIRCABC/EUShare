03651d9118bdfbb81389784e83ae40c8606728c5
catalry <catalry@users.noreply.github.com>
2019-12-02 15:43:21 +0100

    Issue #52 : adding log info

--- docker/server/conf/setenv.sh
+++ docker/server/conf/setenv.sh
@@ -1,1 +1,2 @@
-set JAVA_OPTS=-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8886
+export JAVA_OPTS=-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8886
+echo server debugging port is 8886 see port mapping
