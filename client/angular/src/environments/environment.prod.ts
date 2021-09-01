72d287a1bca6370b72cb1b43d4fb6e85dbc3c0a6
phcollignon <phc@starobject.com>
2021-09-01 22:42:30 +0200

    Move devops resources to private repository

---
+++ client/angular/src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+import { Environment } from './interface';
+export const environment: Environment = {
+  production: false,
+  frontend_url: '/',
+  backend_url: '/webservice',
+  API_BASE_PATH: '/webservice',
+  OIDC_ISSUER: 'https://ecas-mockup.cc.cec.eu.int:7002/cas/oauth2',
+  OIDC_REDIRECTURI: window.location.origin + '/callback',
+  OIDC_CLIENTID: 'CyLq8yleGpc7G1MmxF8la4ckmvzuxN0dodvXht89zzjqXi6geXFTIaNyfpRjAVFMisztmpvxWdXIrLqyNQvErbS-l0EvyMANoKy8j6knSKYyfa',
+  OIDC_BACKEND_CLIENTID: 'VtwvhHcuGQFNUV519SdUj7ZXFztOqUqKyizqyusEHYyeq4zWWg0wlrsCH2C2k2UOdRP7BSzgl5ZCxpC8YsaRQRy-l0EvyMANoKy8j6knSKYyfa',
+  OIDC_TOKENENDPOINT: 'https://ecas-mockup.cc.cec.eu.int:7002/cas/oauth2/token'
+  };
--- client/angular/src/environments/version.ts
+++ client/angular/src/environments/version.ts
@@ -1,14 +1,13 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
-/* tslint:disable */
+/* eslint-disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "31b1b01-dirty",
-    "hash": "31b1b01",
+    "raw": "f9164ba-dirty",
+    "hash": "f9164ba",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "31b1b01-dirty",
+    "suffix": "f9164ba-dirty",
     "semverString": null,
     "version": "0.0.0"
 };
-/* tslint:enable */
