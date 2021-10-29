aaf1d779fd46827d03e355131e607ecba9f6af75
phcollignon <phc@starobject.com>
2021-10-29 11:33:34 +0200

    update license headers + fix com.mycila plugin

--- server/src/main/java/eu/europa/circabc/eushare/exceptions/WrongAuthenticationException.java
+++ server/src/main/java/eu/europa/circabc/eushare/exceptions/WrongAuthenticationException.java
@@ -1,13 +1,12 @@
-/**
- * EasyShare - a module of CIRCABC
- * Copyright (C) 2019 European Commission
+/*
+ * EUShare - a module of CIRCABC
+ * Copyright (C) 2019-2021 European Commission
  *
- * This file is part of the "EasyShare" project.
+ * This file is part of the "EUShare" project.
  *
  * This code is publicly distributed under the terms of EUPL-V1.2 license,
  * available at root of the project or at https://joinup.ec.europa.eu/collection/eupl/eupl-text-11-12.
  */
-
 package eu.europa.circabc.eushare.exceptions;
 
 public class WrongAuthenticationException extends Exception {
