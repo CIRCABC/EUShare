545884cbc50870a63e1ef5ffc35392b00be7d156
topce <topce@users.noreply.github.com>
2021-11-03 13:10:47 +0100

    after code gen add license

--- client/angular/src/app/openapi/api.module.ts
+++ client/angular/src/app/openapi/api.module.ts
@@ -1,3 +1,12 @@
+/*
+EasyShare - a module of CIRCABC
+Copyright (C) 2019 European Commission
+
+This file is part of the "EasyShare" project.
+
+This code is publicly distributed under the terms of EUPL-V1.2 license,
+available at root of the project or at https://joinup.ec.europa.eu/collection/eupl/eupl-text-11-12.
+*/
 import { NgModule, ModuleWithProviders, SkipSelf, Optional } from '@angular/core';
 import { Configuration } from './configuration';
 import { HttpClient } from '@angular/common/http';
