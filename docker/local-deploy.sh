d3d7bb3043d2c57f47c602b46330a334e16b132b
Philippe Collignon <phc@starobject.com>
2022-01-20 16:17:23 +0100

    html fallback

---
+++ docker/local-deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+rm -rf ../client/target/eushareclient.war 
+rm -rf ../server/target/eushareserver.war 
+
+
+pushd ../client/angular
+npm install
+#ng build --configuration=dev --base-href /share/
+ng build --configuration=dev --base-href /share/ # --deploy-url /share/
+popd
+
+
+pushd ../client
+mvn clean install
+popd
+
+pushd ../server
+mvn clean install -Pdev-tomcat -Dskip.unit.tests=true
+popd
+
+rm -rf ../docker/client-tomcat/dist/eushareclient.war
+rm -rf ../docker/server-tomcat/dist/eushareserver.war
+
+cp ../client/target/eushareclient.war ../docker/client-tomcat/dist/eushareclient.war
+cp ../server/target/eushareserver.war ../docker/server-tomcat/dist/eushareserver.war
