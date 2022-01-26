f484fb09f6fe906e39c510217c56a9900da29d4e
Philippe Collignon <phc@starobject.com>
2022-01-26 12:02:43 +0100

    footer update

--- client/angular/src/app/footer/footer.component.ts
+++ client/angular/src/app/footer/footer.component.ts
@@ -1,21 +1,25 @@
-/*
-EasyShare - a module of CIRCABC
-Copyright (C) 2019 European Commission
 
-This file is part of the "EasyShare" project.
 
-This code is publicly distributed under the terms of EUPL-V1.2 license,
-available at root of the project or at https://joinup.ec.europa.eu/collection/eupl/eupl-text-11-12.
-*/
-import { Component } from '@angular/core';
-import { VERSION } from '../../environments/version';
+import { Component, Inject } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+
 
 @Component({
-  selector: 'app-footer',
+  selector: 'cbc-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
+  preserveWhitespaces: true,
 })
 export class FooterComponent {
-  public version = VERSION;
-  public hash = this.version.hash;
+  public appAlfVersion = '';
+  public appVersion = '0.1';
+  public nodeName = '';
+  public buildDate = '';
+  public circabc_url: string = environment.circabc_url;
+  
+  constructor(
+
+  ) {
+  }
 }
