ce9d80b55263a99329b7a5cd8d2b2b48faa918de
Philippe Collignon <phc@starobject.com>
2022-05-17 11:53:15 +0200

    Merge branch 'develop' of https://github.com/CIRCABC/EUShare into develop

--- client/angular/src/environments/environment.dev.ts
+++ client/angular/src/environments/environment.dev.ts
@@ -7,13 +7,11 @@ export const environment: Environment = {
   production: false,
   backend_url: '/share/webservice',
   frontend_url: '/share',
-  circabc_url: 'https://circabc.europa.eu',
+  circabc_url: 'https://circabc.development.europa.eu',
   API_BASE_PATH: '/share/webservice',
   OIDC_ISSUER: 'https://eulogin:7002/cas/oauth2',
   OIDC_REDIRECTURI: `${window.location.origin}/share/callback`,
-  OIDC_CLIENTID:
-    'qeBCzrSrDsizahesBX4QY30a6QYXLj8e1vRTIbqav1ajjnDjSJCPRdcZRwiIITzcbzITc7i8i8y6VZ5CDANzoL2-4y0FDGUzNApE4Hnya956ue',
-  OIDC_BACKEND_CLIENTID:
-    '6QCA7zVJsPSmaZfjsZ5QsjIxpXBmzadfzMjRX5Gc0WFywHmF392po2PjVRgzvWKo7n5qo3c5hyPxFEuItUdM3lK-4y0FDGUzNApE4Hnya956ue',
-  OIDC_TOKENENDPOINT: 'https://eulogin:7002/cas/oauth2/token',
-};
+  OIDC_CLIENTID: 'qeBCzrSrDsizahesBX4QY30a6QYXLj8e1vRTIbqav1ajjnDjSJCPRdcZRwiIITzcbzITc7i8i8y6VZ5CDANzoL2-4y0FDGUzNApE4Hnya956ue',
+  OIDC_BACKEND_CLIENTID: '6QCA7zVJsPSmaZfjsZ5QsjIxpXBmzadfzMjRX5Gc0WFywHmF392po2PjVRgzvWKo7n5qo3c5hyPxFEuItUdM3lK-4y0FDGUzNApE4Hnya956ue',
+  OIDC_TOKENENDPOINT: 'https://eulogin:7002/cas/oauth2/token'
+  };
