f2b168579ea52238d1a564258b2e7f9b5c552853
topce <topce@users.noreply.github.com>
2022-08-16 11:26:19 +0200

    add retries in run mode

--- client/angular/cypress.config.ts
+++ client/angular/cypress.config.ts
@@ -7,10 +7,18 @@ export default defineConfig({
     viewportHeight: 768,
     viewportWidth: 1280,
     chromeWebSecurity: true,
-    defaultCommandTimeout: 8000
+    defaultCommandTimeout: 8000,
   },
   env: {
     euloginServer: 'localhost:7002',
     mailDevServer: 'localhost:1080',
+  },
+  retries: {
+    // Configure retry attempts for `cypress run`
+    // Default is 0
+    runMode: 2,
+    // Configure retry attempts for `cypress open`
+    // Default is 0
+    openMode: 0,
   },
 });
