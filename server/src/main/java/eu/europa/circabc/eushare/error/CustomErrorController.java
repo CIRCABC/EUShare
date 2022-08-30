729555099bdb352bd22e0d89f8207cbf64a315e3
topce <topce@users.noreply.github.com>
2022-08-30 11:09:13 +0200

    log exception in custom error controller

--- server/src/main/java/eu/europa/circabc/eushare/error/CustomErrorController.java
+++ server/src/main/java/eu/europa/circabc/eushare/error/CustomErrorController.java
@@ -10,14 +10,20 @@
 package eu.europa.circabc.eushare.error;
 
 import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Controller
 public class CustomErrorController implements ErrorController {
 
+  private Logger log = LoggerFactory.getLogger(CustomErrorController.class);
+
+  @ApiIgnore
   @RequestMapping("/error")
   @ResponseBody
   public String handleError(HttpServletRequest request) {
@@ -27,6 +33,9 @@ public class CustomErrorController implements ErrorController {
     Exception exception = (Exception) request.getAttribute(
       "javax.servlet.error.exception"
     );
+    if (exception != null) {
+      log.error("Error happened", exception);
+    }
     return String.format(
       "<html><body><h2>Error Page</h2><div>Status code: <b>%s</b></div>" +
       "<div>Exception Message: <b>%s</b></div><body></html>",
