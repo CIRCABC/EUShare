01c0b65bb4c376e12c3af9d3f62e4d33ef60bd30
topce <topce@users.noreply.github.com>
2022-11-15 11:07:44 +0100

    rename variable

--- client/angular/src/app/upload/upload.component.ts
+++ client/angular/src/app/upload/upload.component.ts
@@ -135,7 +135,7 @@ export class UploadComponent implements OnInit {
       ]),
       expirationDate: [this.get7DaysAfterToday(), Validators.required],
       password: [undefined],
-      download_notification: [undefined],
+      downloadNotification: [undefined],
     });
     this.resetEmailMessageArray();
     this.addEmailMessageFormGroup();
@@ -387,7 +387,7 @@ export class UploadComponent implements OnInit {
     return this.uploadform.controls['password'].value;
   }
   getDownloadNotification(): boolean {
-    return  this.uploadform.controls['download_notification'].value;
+    return  this.uploadform.controls['downloadNotification'].value;
   }
   resetPassword(): void {
     this.uploadform.controls['password'].reset();
