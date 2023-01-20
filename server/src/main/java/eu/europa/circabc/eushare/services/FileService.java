f7a423127bc7a6ba19af34da8a55c7e2b2a32b02
topce <topce@users.noreply.github.com>
2023-01-20 16:57:34 +0100

    fix sonar qube issues

--- server/src/main/java/eu/europa/circabc/eushare/services/FileService.java
+++ server/src/main/java/eu/europa/circabc/eushare/services/FileService.java
@@ -515,20 +515,12 @@ public class FileService implements FileServiceInterface {
     DBFile dbFile;
 
     DBShare dbShare;
-    if (downloadId.length() < 10) {
-      dbShare = shareRepository.findOneByShorturl(downloadId);
-    } else {
-      dbShare = shareRepository.findOneByDownloadId(downloadId);
-    }
+    boolean isShortLength = downloadId.length() < 10;
+    dbShare = findShare(downloadId, isShortLength);
 
     if (dbShare == null) {
       // File is downloaded by its uploader
-
-      if (downloadId.length() < 10) {
-        dbFile = findAvailableFileByShortUrl(downloadId);
-      } else {
-        dbFile = findAvailableFile(downloadId, false);
-      }
+      dbFile = findFile(downloadId, isShortLength);
     } else {
       // File is downloaded by a user it is shared with
       dbFile = dbShare.getFile();
@@ -572,6 +564,26 @@ public class FileService implements FileServiceInterface {
     fileLogsRepository.save(fileLogs);
 
     return new DownloadReturn(file, dbFile.getFilename(), dbFile.getSize());
+  }
+
+  private DBFile findFile(String downloadId, boolean isShortLength) throws UnknownFileException {
+    DBFile dbFile;
+    if (isShortLength) {
+      dbFile = findAvailableFileByShortUrl(downloadId);
+    } else {
+      dbFile = findAvailableFile(downloadId, false);
+    }
+    return dbFile;
+  }
+
+  private DBShare findShare(String downloadId, boolean isShortLength) {
+    DBShare dbShare;
+    if (isShortLength) {
+      dbShare = shareRepository.findOneByShorturl(downloadId);
+    } else {
+      dbShare = shareRepository.findOneByDownloadId(downloadId);
+    }
+    return dbShare;
   }
 
   @Override
@@ -783,7 +795,7 @@ public class FileService implements FileServiceInterface {
 
   public void changeDownloadNotificationShareOnFileOnBehalfOf(String fileID, @NotNull @Valid String userEmail,
       String requesterId, Boolean downloadNotification)
-      throws MessagingException, UnknownFileException, UnknownUserException, UserUnauthorizedException {
+      throws UnknownFileException, UnknownUserException, UserUnauthorizedException {
 
     if (this.isRequesterTheOwnerOfTheFileOrIsAnAdmin(fileID, requesterId)) {
 
