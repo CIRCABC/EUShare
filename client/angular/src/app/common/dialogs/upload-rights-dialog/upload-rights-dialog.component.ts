99b3470dfd3d92a3957816b70a7774bae692d7db
Philippe Collignon <phc@starobject.com>
2023-06-09 15:05:11 +0200

    upload dialog

---
+++ client/angular/src/app/common/dialogs/upload-rights-dialog/upload-rights-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-rights-dialog',
+  templateUrl: './upload-rights-dialog.component.html',
+  styleUrls: ['./upload-rights-dialog.component.scss']
+})
+export class UploadRightsDialogComponent {
+
+}
