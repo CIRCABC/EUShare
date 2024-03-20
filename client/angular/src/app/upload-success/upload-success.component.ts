9d57b9db3fe70abbd650fd0816d4d5d3c46fceeb
topce <topce@users.noreply.github.com>
2024-03-20 12:48:03 +0100

    add license in header

--- client/angular/src/app/upload-success/upload-success.component.ts
+++ client/angular/src/app/upload-success/upload-success.component.ts
@@ -1,3 +1,13 @@
+/*
+CIRCABC Share - a module of CIRCABC
+Copyright (C) 2019 European Commission
+
+This file is part of the "CIRCABC Share" project.
+
+This code is publicly distributed under the terms of EUPL-V1.2 license,
+available at root of the project or at https://joinup.ec.europa.eu/collection/eupl/eupl-text-11-12.
+*/
+
 import { Component, OnInit } from '@angular/core';
 import { Clipboard } from '@angular/cdk/clipboard';
 import { NotificationService } from '../common/notification/notification.service';
